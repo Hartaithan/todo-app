d98dc24147e58cb472616f04072f6f09a2364bc4
hartaithan <hartaithan@gmail.com>
2022-05-29 13:40:15 +0900

    add global styles and colors

---
+++ styles/global.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../constants/colors';
+
+export const global = StyleSheet.create({
+  text: {
+    color: colors.gray[100],
+  },
+});
